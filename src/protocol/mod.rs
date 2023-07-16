d03e88c1c168657e843478984a8f9e7e54be0589
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-07-16 12:38:40 +0200

    Move `curl` protocol under separate module
    This is more idiomatic, and allow us to keep `parse_request` agnostic to
    protocol used. In future is also more open to new format.
    Right now RESP is out of the box but we can also opt-in or opt-out in
    some way.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,11 +1,47 @@
+pub mod curl;
 pub mod resp;
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq)]
 pub(crate) enum Command {
-    _Set { key: String, value: String },
-    _Get { key: String },
-    _Del { key: String },
+    /// Get the value of key.
+    /// If the key does not exist the special value nil is returned.
+    /// An error is returned if the value stored at key is not a string, because GET only handles string values.
+    Get {
+        key: String,
+    },
+
+    /// Set key to hold the string value.
+    /// If key already holds a value, it is overwritten, regardless of its type.
+    Set {
+        key: String,
+        value: String,
+    },
+
+    /// Removes the specified keys. A key is ignored if it does not exist.
+    Del {
+        key: String,
+    },
+
+    // FIXME: not sure what it does, but it's the first sent by redis-cli
     COMMAND,
+}
+
+impl Command {
+    fn new(kind: &str, key: &str, value: Option<String>) -> Self {
+        let key = key.trim_matches('/').to_string();
+        match kind {
+            "GET" => Command::Get { key },
+            "POST" | "SET" => match value {
+                Some(v) => Command::Set {
+                    key,
+                    value: v.trim().to_string(),
+                },
+                None => Command::Del { key },
+            },
+            "DELETE" | "DEL" => Command::Del { key },
+            _ => unimplemented!("not implemented"),
+        }
+    }
 }
 
 pub(crate) trait Protocol {
